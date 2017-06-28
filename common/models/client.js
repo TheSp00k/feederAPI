0a068ad21038b09c61f1b4da19165a5f6ce09a27
Tautvydas Kęstutis <kestutis.tautvydas@affecto.com>
2017-06-28 18:37:14 +0300

    changes

--- common/models/client.js
+++ common/models/client.js
@@ -2,4 +2,48 @@
 
 module.exports = (Client) => {
 
+	Client.generateId = () => {
+		var d = new Date().getTime();
+		var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+			var r = (d + Math.random() * 16) % 16 | 0;
+			d = Math.floor(d / 16);
+			return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+		});
+		return uuid;
+	};
+	Client.beforeRemote('create', (ctx, unused, next) => {
+		console.log(ctx.req.body);
+
+		ctx.req.body.appid = Client.generateId();
+		next();
+	});
+	Client.authAppId = (appid, domain, next) => {
+		console.log(appid, domain);
+		
+		Client.findOne({where: {and: [{appid: appid}, {domain: domain}]}}, (err, clientInstance) => {
+			if (err) {
+				return next(err);
+			}
+			if (!clientInstance) {
+				let error = new Error();
+				error.statusCode = 404;
+				error.code = 404;
+				error.message = 'App id is invalid';
+				return next(error);
+			}
+			next(null, clientInstance);
+		});
+	};
+	Client.remoteMethod(
+		'authAppId',
+		{
+			description: 'Checks if app Id is valid',
+			http: { path: '/authappid', verb: 'get' },
+			accepts: [
+				{arg: 'appid', type: 'string', description: 'App id that is sent by the client'},
+				{arg: 'domain', type: 'string', description: 'Domain sent by client for CORS security'},
+			],
+			returns: { arg: 'data', type: 'any', root: true },
+		}
+	);
 };
---
+++ server/db/036-client-appid.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `client` 
+ADD COLUMN `appid` VARCHAR(45) NULL AFTER `showheader`;
