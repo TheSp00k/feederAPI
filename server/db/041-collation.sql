b9f63edb9f4078cf70b604cc5d6b51efc78472e4
Tautvydas Kęstutis <kestutis.tautvydas@affecto.com>
2017-07-17 18:41:52 +0300

    collation

---
+++ server/db/041-collation.sql
@@ -0,0 +1,16 @@
+ALTER TABLE `feedback` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
+ALTER TABLE `appuser` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
+ALTER TABLE `client` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
+ALTER TABLE `customer` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
+ALTER TABLE `product` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
+ALTER TABLE `rawrequest` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
+ALTER TABLE `report` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
+ALTER TABLE `request` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
